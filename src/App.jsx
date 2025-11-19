552762b8d0993ff3ed702995763e913a5b046f6e
RaviGahire <ravigahire3@gmail.com>
2025-11-19 18:35:44 +0530

    Add pages in components folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ function App() {
 
   return (
     <>
-    
+      <h1 className="bg-gray-800 text-3xl font-bold text-white text-center p-5 max-w-7xl mx-auto rounded-xl">Sembark Tech - Frontend React JS Assignment</h1>
+
     </>
   )
 }
---
+++ src/Components/ProductsDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ProductsDetail = () => {
+  return (
+    <div>ProductsDetail</div>
+  )
+}
